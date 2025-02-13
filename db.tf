cc1a2a331077738deeed2d5dced89a3bbfe77f4e
unknown <alonrdt@gmail.com>
2025-02-13 17:27:03 +0200

    [db] saves state between rebuilding of setup

--- db.tf
+++ db.tf
@@ -12,14 +12,15 @@ resource "aws_db_instance" "postgres-events" {
   allocated_storage      = 20
   storage_type           = "gp2"
   engine                 = "postgres"
-  engine_version         = "13"       # adjust to your desired version
-  instance_class         = "db.t3.micro" # change to suit your workload
+  engine_version         = "13"
+  instance_class         = "db.t3.micro"
   username               = local.postgres_credentials.db_username
   password               = local.postgres_credentials.db_password
   db_subnet_group_name   = aws_db_subnet_group.postgres_subnet_group.name
   vpc_security_group_ids = [aws_security_group.db_sg.id]
-  publicly_accessible    = true          # change to false if you want it private
-  skip_final_snapshot    = true          # for testing; for production, you may not want to skip
+  publicly_accessible    = true
+  skip_final_snapshot    = false  # Ensure a final snapshot is taken
+  final_snapshot_identifier = "final-snapshot-${formatdate("YYYYMMDDhhmmss", timestamp())}"
 
   tags = {
     Name = "Postgres Database"
